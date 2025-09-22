5a073e9d7f3e25f09e421d69bd11a8d872317f12
Fatika Azam <fatikahazam@gmail.com>
2025-09-22 15:54:09 +0500

    base structure

---
+++ server.js
@@ -0,0 +1,15 @@
+import http from 'http';
+const PORT = 8000
+const server = http.createServer((req, res) =>{
+    // res.write('HelloWorld')
+    res.setHeader('Content-Type', 'text/html')
+    res.statusCode =404
+    res.end('<h1>hello world</h1>')
+    
+})
+
+
+
+server.listen( PORT ,()=>{
+    console.log(`Server running on port:${PORT} `)
+})
